d5aa351e7e7fd416c29f554287f46829eff9fccd
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-04-22 22:55:28 -0700

    RAINBOW

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import container from './componants/container';
+let menu={
+  backgroundColor:'#ccff3'
+}
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h2>this is the container child</h2>
     </div>
   );
 }
